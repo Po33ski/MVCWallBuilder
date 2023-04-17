93a021ce8699a09c573d355b21a513c7fe166c45
Jarek <jaroslaw.popardowski@wp.pl>
2023-04-17 20:17:18 +0200

    Dodaj pliki projektów.

---
+++ Controllers/FrontController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWallBuilder.Models;
+
+namespace MVCWallBuilder.Controllers
+{
+    public class FrontController : Controller
+    {
+        // list with all layouts of created wall
+        private static List<WallLayoutViewModel> wallLayouts = new List<WallLayoutViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(wallLayouts);
+        }
+        // display the form
+        public IActionResult Create()
+        {
+            var wallLayVm = new WallLayoutViewModel();
+            return View(wallLayVm);
+        }
+        // bound with view
+        public IActionResult CreateLayer(WallLayoutViewModel layoutViewModel)
+        {
+           // add the new layer to the list
+           wallLayouts.Add(layoutViewModel);
+           return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Here return smth";
+        }
+
+    }
+}
---
+++ Models/WallLayoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCWallBuilder.Models
+{
+    public class WallLayoutViewModel
+    {
+        public string Name { get; set;}
+        public float Wide { get; set; }
+        public string LambdaFactor { get; set; }
+        public float LFAfterConv { 
+            get => float.Parse(LambdaFactor); 
+            set => float.Parse(LambdaFactor); }
+
+    }
+}
